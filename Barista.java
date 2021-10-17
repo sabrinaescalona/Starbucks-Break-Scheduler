a6ceab63468814e91132fb8f819318ce0eb167bf
Sabrina Escalona <escalona.sabrina@gmail.com>
2021-10-16 20:31:30 -0700

    NullPointerException from shiftLength()

--- Barista.java
+++ Barista.java
@@ -3,86 +3,101 @@
 * Date: October 15, 2021
 */
 
-import java.time.LocalDateTime;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.time.*;
 
 public class Barista 
-{
-    private double shiftLength;
+{   
+    String shiftStart;
+    String shiftEnd;
+    SimpleDateFormat format = new SimpleDateFormat("HH:MM");
     
     //Barista constructor
-    public Barista(double shiftLength)
+    public Barista(String shiftStart, String shiftEnd, String name)
     {
-        shiftLength = this.shiftLength;
+        shiftStart = this.shiftStart;
+        shiftEnd = this.shiftEnd;
     }  
 
-    private boolean hasLunch()
+    public double shiftLength() throws Exception
     {
-        boolean needsLunch = false;
-        if(shiftLength >= 5.0)
-        {
-            needsLunch = true;
-        }
-        else
-        {
-            needsLunch = false;
-        }
-        return needsLunch;
+        Date start = format.parse(shiftStart);
+        Date end = format.parse(shiftEnd);
+        double shiftLength = (Math.abs(start.getTime() - end.getTime()) / (60*60*1000)) % 24;
+        return shiftLength;
     }
 
-    private int calculateNumLunch()
-    {
-        int numOfLunch = 0;
-        
-        if(hasLunch() == false) //If the shift is 2 hours or less, no break is required
-        {
-            numOfLunch = 0;
-        }
-        else 
-        {
-            numOfLunch = 1;
-        }
-        return numOfLunch;
-    }
-        
-    private int calculateNumTens()
-    {
-        int numOfTens = 0;
-        
-        if(shiftLength <= 2.0) //If the shift is 2 hours or less, no break is required
-        {
-            numOfTens = 0;
-        }
-        else if(hasLunch() == false) //If the shift is less than 5 hours
-        {
-            numOfTens = 1;
-        }
-        else if(shiftLength >= 5.0 && shiftLength < 6.0)
-        {
-            numOfTens = 1;
-        }
-        else 
-        {
-            numOfTens = 2;
-        }
-
-        return numOfTens;
-    }
-
-    private void totalBreaks()
-    {
-        int totalBreaks = calculateNumTens() + calculateNumLunch();
-    }
     
-    private boolean validShift()
-    {
-        if(shiftLength < 0.0 || shiftLength > 10.0)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
+
+    // private boolean hasLunch()
+    // {
+    //     boolean needsLunch = false;
+    //     if(shiftLength >= 5.0)
+    //     {
+    //         needsLunch = true;
+    //     }
+    //     else
+    //     {
+    //         needsLunch = false;
+    //     }
+    //     return needsLunch;
+    // }
+
+    // private int calculateNumLunch()
+    // {
+    //     int numOfLunch = 0;
+        
+    //     if(hasLunch() == false) //If the shift is 2 hours or less, no break is required
+    //     {
+    //         numOfLunch = 0;
+    //     }
+    //     else 
+    //     {
+    //         numOfLunch = 1;
+    //     }
+    //     return numOfLunch;
+    // }
+        
+    // private int calculateNumTens()
+    // {
+    //     int numOfTens = 0;
+        
+    //     if(shiftLength <= 2.0) //If the shift is 2 hours or less, no break is required
+    //     {
+    //         numOfTens = 0;
+    //     }
+    //     else if(hasLunch() == false) //If the shift is less than 5 hours
+    //     {
+    //         numOfTens = 1;
+    //     }
+    //     else if(shiftLength >= 5.0 && shiftLength < 6.0)
+    //     {
+    //         numOfTens = 1;
+    //     }
+    //     else 
+    //     {
+    //         numOfTens = 2;
+    //     }
+
+    //     return numOfTens;
+    // }
+
+    // private void totalBreaks()
+    // {
+    //     int totalBreaks = calculateNumTens() + calculateNumLunch();
+    // }
+    
+    // private boolean validShift()
+    // {
+    //     if(shiftLength < 0.0 || shiftLength > 10.0)
+    //     {
+    //         return false;
+    //     }
+    //     else
+    //     {
+    //         return true;
+    //     }
+    // }
     
 }
--- Main.java
+++ Main.java
@@ -3,10 +3,30 @@
 * Date: October 15, 2021
 */
 
+import java.time.*;
+import java.util.*;
+
 public class Main 
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws Exception
     {
+        Barista bar1 = new Barista("5:30", "9:30", "Sabrina");
+        System.out.println("Test of shift length of bar1: " + bar1.shiftLength());
+        
 
+
+        
+
+        // int numBaristas = 0;
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Please enter the number of baristas who are on your floor today: ");
+        // numBaristas = scan.nextInt();
+
+        // for(int i = 0; i < numBaristas; i++)
+        // {
+        //     System.out.println("");
+        // }
+
+        // scan.close();
     }
 }
